43fab218c1b2fcb9c76b494877a01366c6a3d2d0
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-01-15 14:11:08 -0600

    Lab2
    - Exercise written
    - Exercise odd/even

---
+++ Lab2/exerciseOddEven_2.js
@@ -0,0 +1,28 @@
+
+Array.prototype.even = function(){
+    setImmediate(() => {
+        let result = [];
+        this.forEach(element => {
+            if(element %2 == 0)
+                result.push(element);
+        });
+        console.log(result);
+    })
+}
+
+Array.prototype.odd = function(){
+    setImmediate(() => {
+        result = [];
+        this.forEach(element => {
+            if(element %2 != 0)
+                result.push(element);
+        });
+        console.log(result);
+    })
+}
+
+
+console.log('start');
+[1, 2, 3, 4, 5, 6 ,7, 8].even();
+[1, 2, 3, 4, 5, 6, 7, 8].odd();
+console.log('end');

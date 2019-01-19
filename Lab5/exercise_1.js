61e8c7cc992b881c0ff841d6f699564c786d1419
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-01-18 21:48:43 -0600

    add pagination from url

--- Lab5/exercise_1.js
+++ Lab5/exercise_1.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const express = require('express');
+var router = express.Router();
 const app = express();
-const linkUrl = 'https://randomuser.me/api/?results=10';
+//const linkUrl = 'https://randomuser.me/api/?results=1';
+const linkUrl = 'https://randomuser.me/api/?results=';
 
 app.set('strict routing', true);
 app.set('trust proxy', true);
@@ -12,9 +14,18 @@ app.use(express.static(__dirname, {
     maxAge: '1d'
 }));
 
+router.get('/', function(req, res) {
+    res.render("index", {"title": "Express"});
+});
+
 app.get('/users', function(request, response){
+    const page = request.query.results;
+    var url ='https://randomuser.me/api/?results=10';
+    if(page)
+        var url = 'https://randomuser.me/api/?results='+page;
+    //console.log(url);
     const getData = async function(res) {
-       await axios.get(linkUrl).then(res => {
+       await axios.get(url).then(res => {
             response.status(200);
             response.set('Content-Type', 'application/json');
             response.send(res.data);
